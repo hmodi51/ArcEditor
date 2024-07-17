8be3091e24ce3ec6a77e2e7469ded9f0ca720f27
hmodi51 <hmodi5@uic.edu>
2024-07-17 13:55:06 -0500

    added bkspc and arrow key features

--- main.cpp
+++ main.cpp
@@ -11,12 +11,29 @@ int main()
     noecho();
     while((ch=getch())!=KEY_F(1))
     {
-
         switch(ch){
             case KEY_BACKSPACE :
-                 printw("%c" , ch);
+                 int x , y;
+                 getyx(stdscr , y , x);
+                 move(y,x-1);
                  delch();
                  break;
+            case KEY_UP :
+                  getyx(stdscr , y , x);
+                  move(y-1,x);
+                  break;
+             case KEY_DOWN :
+                  getyx(stdscr , y , x);
+                  move(y+1,x);
+                  break;
+             case KEY_RIGHT :
+                  getyx(stdscr , y , x);
+                  move(y,x+1);
+                  break;
+              case KEY_LEFT :
+                  getyx(stdscr , y , x);
+                  move(y,x-1);
+                  break;
             default:
                   printw("%c" , ch);
         }
