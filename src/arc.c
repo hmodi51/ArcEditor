e100f5f2094b8a14e07c8485f0cba7aff9b7b345
hmodi51 <modiharshit01@gmail.com>
2025-01-20 22:24:07 -0600

    raw mode and echo mode implemented

--- src/arc.c
+++ src/arc.c
@@ -1,6 +1,30 @@
-#include <stdio.h>
+
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+
+
+struct termios original;
+
+
+void funcRawModeDisabled(){
+   tcsetattr(STDIN_FILENO , TCSAFLUSH , &original);
+}
+
+void funcRawModeEnabled(){
+   struct termios noecho;
+   tcgetattr(STDIN_FILENO , &original);
+   atexit(funcRawModeDisabled);
+   noecho = original;
+   noecho.c_lflag = noecho.c_lflag ^ ECHO;
+   tcsetattr(STDIN_FILENO , TCSAFLUSH , &noecho);
+   
+
+}
 
 int main() {
-  printf("%s" , "ndjnce");
+  funcRawModeEnabled();
+  char c;
+  while (read(STDIN_FILENO , &c , 1) == 1 && c!= 'q');
   return 0;
 }
