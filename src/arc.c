b8b027177ba1ac70e70a69f0dbaf8c94e4c8d5fa
hmodi51 <modiharshit01@gmail.com>
2025-01-21 19:32:01 -0600

    making some changes

--- src/arc.c
+++ src/arc.c
@@ -2,22 +2,25 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+
 
 
 struct termios original;
 
 
 void funcRawModeDisabled(){
-   tcsetattr(STDIN_FILENO , TCSAFLUSH , &original);
+   tcsetattr(STDIN_FILENO , TCSAFLUSH , &original);   // set the terminal attribute back to as it was before starting the program
 }
 
 void funcRawModeEnabled(){
    struct termios noecho;
    tcgetattr(STDIN_FILENO , &original);
-   atexit(funcRawModeDisabled);
+   atexit(funcRawModeDisabled);                       // at exit is called when the program terminated and calls the function argument
    noecho = original;
-   noecho.c_lflag = noecho.c_lflag ^ ( ECHO | ICANON );
-   tcsetattr(STDIN_FILENO , TCSAFLUSH , &noecho);
+   noecho.c_lflag = noecho.c_lflag ^ ( ECHO | ICANON );  //disables the terminal functions like ECHO and ICANON
+   tcsetattr(STDIN_FILENO , TCSAFLUSH , &noecho);  
    
 
 }
@@ -26,5 +29,6 @@ int main() {
   funcRawModeEnabled();
   char c;
   while (read(STDIN_FILENO , &c , 1) == 1 && c!= 'q');
+  printf("%d " , c);
   return 0;
 }
